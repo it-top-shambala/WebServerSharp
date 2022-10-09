3dbb7a369db98f56553e762fc9f4679461cad621
Starinin Andrey <starininandrey@gmail.com>
2022-10-09 14:20:34 +0300

    Update WebServerSharp.App

--- WebServerSharp.App/Program.cs
+++ WebServerSharp.App/Program.cs
@@ -1,3 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using WebServerSharp.HttpLib;
 
-Console.WriteLine("Hello, World!");
+var server = new WebServer();
+await server.StartAsync();
