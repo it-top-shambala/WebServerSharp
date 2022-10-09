92e3a32300d57c48036c15ad6a49968550e0505a
Starinin Andrey <starininandrey@gmail.com>
2022-10-09 12:18:58 +0300

    Init ConfigLib proj

---
+++ WebServerSharp.ConfigLib/WebServerSharpConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Text.Json;
+
+namespace WebServerSharp.ConfigLib;
+
+public class WebServerSharpConfig
+{
+    public string Scheme { get; set; }
+    public string Host { get; set; }
+    public int Port { get; set; }
+    public string LocalPath { get; set; }
+
+    public WebServerSharpConfig()
+    {
+        Scheme = "http";
+        Host = "localhost";
+        Port = 8005;
+        LocalPath = @"C:\WebServerSharp";
+    }
+
+    public static WebServerSharpConfig ImportFromJson(string path = "config.json")
+    {
+        if (!File.Exists(path)) return new WebServerSharpConfig();
+
+        try
+        {
+            var json = File.ReadAllText(path);
+            var config = JsonSerializer.Deserialize<WebServerSharpConfig>(json);
+            return config;
+        }
+        catch (Exception)
+        {
+            return new WebServerSharpConfig();
+        }
+    }
+}
